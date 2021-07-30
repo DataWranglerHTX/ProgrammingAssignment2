c46b9438fa6dcfaab59092cf11c98fbca80e1a32
DataWranglerHTX <32746152+DataWranglerHTX@users.noreply.github.com>
2021-07-30 15:23:34 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,setmean = setmean,getmean = getmean)
+}
 
-## Write a short comment describing this function
+## 
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
+
+## makeCacheMatrix funtion
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- mean
+  getinverse <- function() m
+  list(set = set, get = get,setinverse = setinverse,getinverse = getinverse)}
 
-}
-
-
-## Write a short comment describing this function
+## cacheSolve function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
+
+b <- cbind(c(2,1,3), c(0,1,7), c(1,-4,-3))
+a <- matrix(b, nrow = 3, ncol = 3)
+solve(a)
+print(a)
+print(makeCacheMatrix(a))
+print(cacheSolve(makeCacheMatrix(a)))
+print(makeVector(matrix(2, nrow = 3, ncol = 3)))
